cad7def4b53ef3088af199a3fbb83c34197c0c43
Jennifer <jennifer.rodrigues39@etec.sp.gov.br>
2025-08-05 22:49:31 -0300

    Introdução NextJs

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+export default function Home () {
+  return(
+    <div>
+      <h1>Hello World!!!</h1>
+      <p>Primeiro exemplo do Next</p>
+
+      <div className="quadrado"></div>
+    </div>
+
+   
+  );
+}
