dfb393af5d21e392dd223c1c16e5302f0614f801
Jennifer <jennifer.rodrigues39@etec.sp.gov.br>
2025-08-07 22:49:39 -0300

    Aula mobile

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,15 @@
+import style from './page.module.css';
 export default function Home () {
   return(
-    <div>
-      <h1>Hello World!!!</h1>
-      <p>Primeiro exemplo do Next</p>
-
+    <div className="container">
+      <h1 className={style.tituloHome}>Título 1</h1>
+      <h2>Título 2</h2>
+      <h3>Título 3</h3>
+      
+      <p className="txtDestaque">Primeiro exemplo do Next</p>
+      <p>Primeiro exemplo Next</p>
+      <small>Texto menor</small>
+      <br />
       <div className="quadrado"></div>
     </div>
 
